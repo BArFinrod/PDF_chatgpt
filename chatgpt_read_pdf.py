e4e4033300617403d76c7a5978647b18a15e83be
BArFinrod <68872465+BArFinrod@users.noreply.github.com>
2023-04-10 13:35:56 -0500

    Update chatgpt_read_pdf.py

--- chatgpt_read_pdf.py
+++ chatgpt_read_pdf.py
@@ -2,6 +2,7 @@ from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
+from pathlib import Path
 
 import os
 import streamlit as st
